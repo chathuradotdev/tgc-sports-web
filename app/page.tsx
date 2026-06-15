665f0d3179d601ba44c8c6b752d689dc000d7179
hichathura <chathucj2@gmail.com>
2026-06-15 15:37:10 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex items-center gap-4">
-        <Image
-      src="/billiard_logo.jpg"
-      width={300}
-      height={300}
-      alt="Picture of the author"
-    />
-          
+          <Image
+            src="/billiard_logo.jpg"
+            width={300}
+            height={300}
+            alt="Picture of the author"
+          />
+
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
@@ -21,4 +21,4 @@ export default function Home() {
     </div>
   );
 }
-/* test */
+/* test 22 2 */
